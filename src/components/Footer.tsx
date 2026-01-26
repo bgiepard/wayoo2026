62900a0a3f1ca8dd1a131bc587c7de04df3473d1
brtkgprd <bartekgiepard@gmail.com>
2026-01-26 20:20:07 +0100

    Rebuild

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center p-4 border-t border-gray-300 max-w-[1250px] mx-auto">
-      <span>© 2026 wayoo. Wszelkie prawa zastrzeżone.</span>
+      <span>© 2026 Wayoo.pl Wszelkie prawa zastrzeżone.</span>
     </footer>
   );
 }
